9464e6ddf4334f61a32cf4b4360e4ecc095630ce
João Filipe <joao1filipe@outlook.com>
2023-02-03 15:27:49 -0300

    popular movies done

---
+++ src/hooks/useGetPopularMovies.js
@@ -0,0 +1,22 @@
+// api
+import { apiURL, apiKey } from "../api/ApiKey"
+
+// hooks
+import { useEffect, useState } from "react"
+
+export const useGetPopularMovies = () => {
+    const [movies, setMovies] = useState(null)
+
+    useEffect(() => {
+        
+        const fetchMovies = () => {
+            fetch(`${apiURL}/popular?api_key=${apiKey}&language=en-US&page=1`)
+                            .then(res => res.json())
+                            .then(data => {
+                                setMovies(data)
+                            })
+        }
+        fetchMovies()
+    }, [])
+    return movies
+}
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,36 @@
+import "./Home.css"
+
+
+// hooks
+import { useGetPopularMovies } from "../../hooks/useGetPopularMovies"
+import { useState, useEffect } from "react"
+
+// image URL
+const imgURL = "https://image.tmdb.org/t/p/w500"
+
+const Home = () => {
+
+
+  const apiMovies = useGetPopularMovies()
+
+
+  return (
+    <div id="home">
+        
+        <h1>Movies</h1>
+
+        <ul>
+          {apiMovies && apiMovies.results.map(movie => (
+            <li key={movie.id}>
+              <a href="https://google.com.br"><img src={`${imgURL}${movie.poster_path}`} alt="img" /></a>
+              <span>{movie.title}</span>
+            </li>
+          ))}
+        </ul>
+
+    </div>
+  )
+}
+
+
+export default Home
